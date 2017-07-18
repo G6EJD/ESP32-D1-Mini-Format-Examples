cbb0ff5cbffb57a903bca592670c29257b9e735d
G6EJD <d.l.bird@sky.com>
2017-07-18 17:17:29 +0100

    Add files via upload

---
+++ ESP32_BMP180_D1_variant.ino
@@ -0,0 +1,69 @@
+#include <Wire.h>
+#include <Adafruit_BMP085.h>
+
+/*************************************************** 
+  This is an example for the BMP085 Barometric Pressure & Temp Sensor
+
+  Designed specifically to work with the Adafruit BMP085 Breakout 
+  ----> https://www.adafruit.com/products/391
+
+  These displays use I2C to communicate, 2 pins are required to  
+  interface
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
+
+// Connect VCC of the BMP085 sensor to 3.3V (NOT 5.0V!)
+// Connect GND to Ground
+// Connect SCL to i2c clock - on an ESP32 that's GPIO 21
+// Connect SDA to i2c data  - on an ESP32 that's GPIO 22
+
+const int SCLpin = 21;
+const int SDApin = 22;
+
+Adafruit_BMP085 bmp;
+  
+void setup() {
+  Serial.begin(115200);
+  Wire.begin(SCLpin,SDApin);
+  Serial.println("Searching for BMP Sensor");
+  if (!bmp.begin()) {
+	  Serial.println("Could not find a valid BMP(180/085) sensor, check wiring!");
+	while (1) {}
+  }
+}
+  
+void loop() {
+    Serial.print("Temperature = ");
+    Serial.print(bmp.readTemperature());
+    Serial.println("'C");
+    
+    Serial.print("Pressure = ");
+    Serial.print(float((bmp.readPressure() + 600))/100,2);
+    Serial.println(" Pa");
+    
+    // Calculate altitude assuming 'standard' barometric
+    // pressure of 1013.25 millibar = 101325 Pascal
+    Serial.print("Altitude = ");
+    Serial.print(bmp.readAltitude());
+    Serial.println(" meters");
+
+    Serial.print("Pressure at sealevel (calculated) = ");
+    Serial.print(bmp.readSealevelPressure());
+    Serial.println(" Pa");
+
+  // you can get a more precise measurement of altitude
+  // if you know the current sea level pressure which will
+  // vary with weather and such. If it is 1015 millibars
+  // that is equal to 101500 Pascals.
+    Serial.print("Real altitude = ");
+    Serial.print(bmp.readAltitude(101500));
+    Serial.println(" meters");
+    
+    Serial.println();
+    delay(5000);
+}
---
+++ ESP32_DHT11_D1_variant.ino
@@ -0,0 +1,28 @@
+// DHT11 Temperature and Humidity Sensors Example
+#include "DHT.h"         //include DHT library
+#define DHTPIN 16        //define as DHTPIN the Pin 16 used to connect the Sensor
+#define DHTTYPE DHT11    //define the sensor used(DHT11)
+DHT dht(DHTPIN, DHTTYPE);//create an instance of DHT
+/*setup*/
+void setup() {
+  Serial.begin(115200);    //initialize the Serial communication
+  delay(6000);           //wait 6 seconds
+  Serial.println("Temperature and Humidity test!");//print on Serial monitor
+  Serial.println("T(C) \tH(%)");                   //print on Serial monitor
+  dht.begin();           //initialize the Serial communication
+}
+/*loop*/
+void loop() {
+ 
+  float h = dht.readHumidity();    // reading Humidity 
+  float t = dht.readTemperature(); // read Temperature as Celsius (the default)
+  // check if any reads failed and exit early (to try again).
+    if (isnan(h) || isnan(t)) {    
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  Serial.print(t, 2);    //print the temperature
+  Serial.print("\t");
+  Serial.println(h, 2);  //print the humidity
+  delay(2000);           //wait 2 seconds
+}
